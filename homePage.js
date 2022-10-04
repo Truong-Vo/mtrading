742e929a21440f7ad762c7ed94f61164d42483d5
Truonq <vqtdnn@DESKTOP-PN2GIUS.localdomain>
2022-10-05 02:52:21 +0700

    3

---
+++ homePage.js
@@ -0,0 +1,17 @@
+// header
+load('./components/header/header.html', $('#app'));
+load('./components/header/header-menu/header-menu.html', $('.header .container'));
+load('./components/header/header-submenu/header-submenu.html', $('.header .container'));
+load('./components/header/header-slide/header-slide.html', $('.header'));
+// benefits
+load('./components/benefits/benefits.html', $('#app'));
+// coppytrade
+load('./components/coppytrade/coppytrade.html', $('#app'));
+// deposit
+load('./components/deposit/deposit.html', $('#app'));
+// patnership
+load('./components/partnership/partnership.html', $('#app'));
+// news
+load('./components/news/news.html', $('#app'));
+// footer
+load('./components/footer/footer.html', $('#app'));
