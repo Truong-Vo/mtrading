61101781450d6e189a974376f6912afcbbb535a7
Truonq <vqtdnn@DESKTOP-PN2GIUS.localdomain>
2022-10-23 23:23:13 +0700

    modify-js

--- js/headerEvents.js
+++ js/headerEvents.js
@@ -1,114 +1,111 @@
 const headerLanguages = $('.header-language__list');
 const showMenu = $$('.header-mobile__show');
 
-const languages = [
-    {
-        linkpage: 'https://mtrading.io/setlocale/id-ID',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/id.gif',
-        language: 'Bahasa (Indonesia)',
-    },
-    {
-        linkpage: 'https://mtrading.io/setlocale/in-EN',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/in.gif',
-        language: 'English (India)',
-    },
-    {
-        linkpage: 'https://mtrading.io/setlocale/es-ES',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/es.gif',
-        language: 'Español (Latinoamérica)',
-    },
-    {
-        linkpage: 'https://mtrading.io/setlocale/ru-RU',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/ru.gif',
-        language: 'Русский (СНГ)',
-    },
-    {
-        linkpage: 'https://mtrading.io/setlocale/vi-VN',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/vn.gif',
-        language: 'Tiếng Việt (Việt Nam)',
-    },
-    {
-        linkpage: 'https://mtrading.io/setlocale/th-TH',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/th.gif',
-        language: 'ไทย (ประเทศไทย)',
-    },
-    {
-        linkpage: 'https://mtrading.io/setlocale/zh-CN',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/cn.gif',
-        language: '简体中文 (中国)',
-    },
-    {
-        linkpage: 'https://mtrading.io/setlocale/ms-MS',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/ms.gif',
-        language: 'Malay',
-    },
-    {
-        linkpage: 'https://mtrading.io/setlocale/pt-PT',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/ms.gif',
-        language: 'Portuguese',
-    },
-    {
-        linkpage: 'https://mtrading.io/setlocale/fil-PH',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/ph.gif',
-        language: 'Filipino',
-    },
-    {
-        linkpage: 'https://mtrading.io/setlocale/en-US',
-        flag: 'https://mtr-cdn.com/img/mtweb/flags/en.gif',
-        language: 'English (International)',
-    },
-];
-
-function loadLanguages() {
-    const htmls = languages.map((language) => {
-        return `
-                <li class="header-language__item">
-            <a href="${language.linkpage}">
-            <img class="header-language__flag"src="${language.flag}" alt=""/>
-            <span class="header-language__label">${language.language}</span>
-            </a>
-            </li>
-            `;
-    });
-    headerLanguages.innerHTML = htmls.join('');
-}
-
-loadLanguages();
-
-function showMenuMobile() {
-    if (showMenu) {
-        showMenu.forEach((element) => {
-            element.onclick = function () {
-                const itemActive = $('.header-mobile__show.active');
-                if (element.classList.contains('active')) {
-                    element.classList.remove('active');
-                } else if (itemActive) {
-                    itemActive.classList.remove('active');
-                    element.classList.add('active');
-                } else {
-                    element.classList.add('active');
-                }
-            };
+const header = {
+    languages: [
+        {
+            linkpage: 'https://mtrading.io/setlocale/id-ID',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/id.gif',
+            language: 'Bahasa (Indonesia)',
+        },
+        {
+            linkpage: 'https://mtrading.io/setlocale/in-EN',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/in.gif',
+            language: 'English (India)',
+        },
+        {
+            linkpage: 'https://mtrading.io/setlocale/es-ES',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/es.gif',
+            language: 'Español (Latinoamérica)',
+        },
+        {
+            linkpage: 'https://mtrading.io/setlocale/ru-RU',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/ru.gif',
+            language: 'Русский (СНГ)',
+        },
+        {
+            linkpage: 'https://mtrading.io/setlocale/vi-VN',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/vn.gif',
+            language: 'Tiếng Việt (Việt Nam)',
+        },
+        {
+            linkpage: 'https://mtrading.io/setlocale/th-TH',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/th.gif',
+            language: 'ไทย (ประเทศไทย)',
+        },
+        {
+            linkpage: 'https://mtrading.io/setlocale/zh-CN',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/cn.gif',
+            language: '简体中文 (中国)',
+        },
+        {
+            linkpage: 'https://mtrading.io/setlocale/ms-MS',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/ms.gif',
+            language: 'Malay',
+        },
+        {
+            linkpage: 'https://mtrading.io/setlocale/pt-PT',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/ms.gif',
+            language: 'Portuguese',
+        },
+        {
+            linkpage: 'https://mtrading.io/setlocale/fil-PH',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/ph.gif',
+            language: 'Filipino',
+        },
+        {
+            linkpage: 'https://mtrading.io/setlocale/en-US',
+            flag: 'https://mtr-cdn.com/img/mtweb/flags/en.gif',
+            language: 'English (International)',
+        },
+    ],
+    // render
+    render: function () {
+        const htmls = this.languages.map((language) => {
+            return `
+                    <li class="header-language__item">
+                <a href="${language.linkpage}">
+                <img class="header-language__flag"src="${language.flag}" alt=""/>
+                <span class="header-language__label">${language.language}</span>
+                </a>
+                </li>
+                `;
         });
-    }
-}
+        headerLanguages.innerHTML = htmls.join('');
+    },
 
-showMenuMobile();
+    // handleEvents
 
-function handleEvents() {
-    const _this = this;
-    // toggle show menu on mobile
-    $('.header-mobile__menu').onclick = function () {
-        this.classList.toggle('open');
-        $('.header-overlay').classList.toggle('active');
-    };
+    handleEvents: function () {
+        const _this = this;
+        // show header on mobile
+        if (showMenu) {
+            showMenu.forEach((element) => {
+                element.onclick = function () {
+                    const itemActive = $('.header-mobile__show.active');
+                    if (element.classList.contains('active')) {
+                        element.classList.remove('active');
+                    } else if (itemActive) {
+                        itemActive.classList.remove('active');
+                        element.classList.add('active');
+                    } else {
+                        element.classList.add('active');
+                    }
+                };
+            });
+        }
 
-    // toggle show authentication
-    // if (auths) {
-    //     $('.register-tabs__login.active').onclick = function () {
-    //         console.log('a');
-    //         this.classList.remove('active');
-    //     };
-    // }
-}
-handleEvents();
+        // toggle show menu on mobile
+        $('.header-mobile__menu').onclick = function () {
+            this.classList.toggle('open');
+            $('.header-overlay').classList.toggle('active');
+        };
+    },
+
+    // start
+    start: function () {
+        this.render();
+        this.handleEvents();
+    },
+};
+header.start();
--- js/margetInsightEvent.js
+++ js/margetInsightEvent.js
@@ -1,112 +1,122 @@
 const list = $('.marketInsight-list');
-const margetInsight = [
-    {
-        link: '',
-        image: '../assets/images/EURUSDH4-12102022.2e16d0ba.fill-830x415.png',
-        author: 'Denis Sergienko',
-        date: '2022-10-12',
-        desc: 'Technical Analysis: EURUSD braces for fresh yearly low ahead of key events/data',
-    },
-    {
-        link: '',
-        image: '../assets/images/GBPUSDH4.2e16d0ba.fill-830x415.png',
-        author: 'Denis Sergienko',
-        date: '2022-10-11',
-        desc: 'Technical Analysis: GBPUSD braces for further downside, 1.0930-20 eyed immediately',
-    },
-    {
-        link: '',
-        image: '../assets/images/BRENTDaily-10102022.2e16d0ba.fill-830x415.png',
-        author: 'MTrading Team',
-        date: '2022-10-10',
-        desc: 'Technical Analysis: Brent oil buyers need successful break of $100 to keep control',
-    },
-    {
-        link: '',
-        image: '../assets/images/GOLDDaily-07102022.2e16d0ba.fill-830x415.png',
-        author: 'MTrading Team',
-        date: '2022-10-07',
-        desc: 'Technical Analysis: Gold sellers need validation from $1,685 and US NFP',
-    },
-    {
-        link: '',
-        image: '../assets/images/GBPUSDH4-06102022.2e16d0ba.fill-830x415.png',
-        author: 'Denis Sergienko',
-        date: '2022-10-05',
-        desc: 'Technical Analysis: NZDUSD struggles to justify RBNZ’s eighth rate hike',
-    },
-    {
-        link: '',
-        image: '../assets/images/AUDUSDH4-04102022.2e16d0ba.fill-830x415.png',
-        author: 'MTrading Team',
-        date: '2022-10-04',
-        desc: 'Technical Analysis: AUDUSD drops towards 0.6500 on RBA’s smaller rate hike',
-    },
-    {
-        link: '',
-        image: '../assets/images/EURUSDH4-03102022.2e16d0ba.fill-830x415.png',
-        author: 'Denis Sergienko',
-        date: '2022-10-03',
-        desc: 'Technical Analysis: EURUSD rebound is at test near 0.9830 resistance',
-    },
-    {
-        link: '',
-        image: '../assets/images/GOLDH4-30092022.2e16d0ba.fill-830x415.png',
-        author: 'Denis Sergienko',
-        date: '2022-09-30',
-        desc: 'Technical Analysis: Gold consolidates monthly losses inside bearish channel',
-    },
-    {
-        link: '',
-        image: '../assets/images/AUDUSDDaily-29092022.2e16d0ba.fill-830x415.png',
-        author: 'Denis Sergienko',
-        date: '2022-09-29',
-        desc: 'Technical Analysis: AUDUSD bears again aim for sub-0.6400 area',
-    },
-    {
-        link: '',
-        image: '../assets/images/GOLDDaily-07102022.2e16d0ba.fill-830x415.png',
-        author: 'Denis Sergienko',
-        date: '2022-10-05',
-        desc: 'The # 2 Swiss Bank Is in Deep Trouble',
-    },
-    {
-        link: '',
-        image: '../assets/images/USDJPYDaily-28092022.2e16d0ba.fill-830x415.png',
-        author: 'Denis Sergienko',
-        date: '2022-09-28',
-        desc: 'USDJPY bulls are bracing for 147.00',
-    },
-    {
-        link: '',
-        image: '../assets/images/GBPUSDDaily-27092022.2e16d0ba.fill-830x415.png',
-        author: 'Denis Sergienko',
-        date: '2022-09-27',
-        desc: 'Technical Analysis: GBPUSD stays on bear’s radar unless crossing 1.1280',
-    },
-];
 
-function renderMargetInsight() {
-    const items = margetInsight.map((item) => {
-        return `
-        <li class="marketInsight-item">
-            <a href="#!">
-                <img
-                    src=${item.image}
-                    alt=""
-                    class="marketInsight-item__image"
-                />
+const marketInsight = {
+    margetInsight: [
+        {
+            link: '',
+            image: '../assets/images/EURUSDH4-12102022.2e16d0ba.fill-830x415.png',
+            author: 'Denis Sergienko',
+            date: '2022-10-12',
+            desc: 'Technical Analysis: EURUSD braces for fresh yearly low ahead of key events/data',
+        },
+        {
+            link: '',
+            image: '../assets/images/GBPUSDH4.2e16d0ba.fill-830x415.png',
+            author: 'Denis Sergienko',
+            date: '2022-10-11',
+            desc: 'Technical Analysis: GBPUSD braces for further downside, 1.0930-20 eyed immediately',
+        },
+        {
+            link: '',
+            image: '../assets/images/BRENTDaily-10102022.2e16d0ba.fill-830x415.png',
+            author: 'MTrading Team',
+            date: '2022-10-10',
+            desc: 'Technical Analysis: Brent oil buyers need successful break of $100 to keep control',
+        },
+        {
+            link: '',
+            image: '../assets/images/GOLDDaily-07102022.2e16d0ba.fill-830x415.png',
+            author: 'MTrading Team',
+            date: '2022-10-07',
+            desc: 'Technical Analysis: Gold sellers need validation from $1,685 and US NFP',
+        },
+        {
+            link: '',
+            image: '../assets/images/GBPUSDH4-06102022.2e16d0ba.fill-830x415.png',
+            author: 'Denis Sergienko',
+            date: '2022-10-05',
+            desc: 'Technical Analysis: NZDUSD struggles to justify RBNZ’s eighth rate hike',
+        },
+        {
+            link: '',
+            image: '../assets/images/AUDUSDH4-04102022.2e16d0ba.fill-830x415.png',
+            author: 'MTrading Team',
+            date: '2022-10-04',
+            desc: 'Technical Analysis: AUDUSD drops towards 0.6500 on RBA’s smaller rate hike',
+        },
+        {
+            link: '',
+            image: '../assets/images/EURUSDH4-03102022.2e16d0ba.fill-830x415.png',
+            author: 'Denis Sergienko',
+            date: '2022-10-03',
+            desc: 'Technical Analysis: EURUSD rebound is at test near 0.9830 resistance',
+        },
+        {
+            link: '',
+            image: '../assets/images/GOLDH4-30092022.2e16d0ba.fill-830x415.png',
+            author: 'Denis Sergienko',
+            date: '2022-09-30',
+            desc: 'Technical Analysis: Gold consolidates monthly losses inside bearish channel',
+        },
+        {
+            link: '',
+            image: '../assets/images/AUDUSDDaily-29092022.2e16d0ba.fill-830x415.png',
+            author: 'Denis Sergienko',
+            date: '2022-09-29',
+            desc: 'Technical Analysis: AUDUSD bears again aim for sub-0.6400 area',
+        },
+        {
+            link: '',
+            image: '../assets/images/GOLDDaily-07102022.2e16d0ba.fill-830x415.png',
+            author: 'Denis Sergienko',
+            date: '2022-10-05',
+            desc: 'The # 2 Swiss Bank Is in Deep Trouble',
+        },
+        {
+            link: '',
+            image: '../assets/images/USDJPYDaily-28092022.2e16d0ba.fill-830x415.png',
+            author: 'Denis Sergienko',
+            date: '2022-09-28',
+            desc: 'USDJPY bulls are bracing for 147.00',
+        },
+        {
+            link: '',
+            image: '../assets/images/GBPUSDDaily-27092022.2e16d0ba.fill-830x415.png',
+            author: 'Denis Sergienko',
+            date: '2022-09-27',
+            desc: 'Technical Analysis: GBPUSD stays on bear’s radar unless crossing 1.1280',
+        },
+    ],
 
-                <p class="marketInsight-item__author">
-                    <span>${item.author}</span>
-                    <span>${item.date}</span>
-                </p>
-                <p class="marketInsight-item__desc">${item.desc}</p>
-            </a>
-            </li>
-        `;
-    });
-    list.innerHTML = items.join('');
-}
+    render: function () {
+        const items = this.margetInsight.map((item) => {
+            return `
+            <li class="marketInsight-item">
+                <a href="#!">
+                    <img
+                        src=${item.image}
+                        alt=""
+                        class="marketInsight-item__image"
+                    />
 
-renderMargetInsight();
+                    <p class="marketInsight-item__author">
+                        <span>${item.author}</span>
+                        <span>${item.date}</span>
+                    </p>
+                    <p class="marketInsight-item__desc">${item.desc}</p>
+                </a>
+                </li>
+            `;
+        });
+        list.innerHTML = items.join('');
+    },
+
+    handleEvents: function () {},
+
+    start: function () {
+        this.render();
+        this.handleEvents();
+    },
+};
+
+marketInsight.start();
